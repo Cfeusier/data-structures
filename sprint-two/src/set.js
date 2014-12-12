972f7b701252f2518ebe66831a13570db08d5fa7
Cfeusier <cfeusier@gmail.com>
2014-12-12 15:48:24 -0800

    Finish set implementation

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,30 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (!this.contains(item)) {
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item){
+  return !this._storage[item] ? false : true;
 };
 
 setPrototype.remove = function(item){
+  if (this.contains(item)) {
+    delete this._storage[item];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *  add O(1)
+ *  contains O(1)
+ *  remove O(1)
  */
