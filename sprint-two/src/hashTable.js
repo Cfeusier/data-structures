0ad8a1eef9a6c95e1951a8d686420038b5e09385
Cfeusier <cfeusier@gmail.com>
2014-12-13 14:34:34 -0800

    Finish resizing hashtable refactor

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,6 @@
 var HashTable = function(){
   this._limit = 8;
+  this._count = 0;
   this._storage = LimitedArray(this._limit);
 };
 
@@ -29,6 +30,10 @@ HashTable.prototype.insert = function(k, v){
   // no matching key found
   if (!found) {
     bucket.push([k,v]);
+    this._count++;
+    if (this._count > this._limit * 0.75) {
+      this._resize(this._limit * 2);
+    }
   }
 
 };
@@ -60,11 +65,34 @@ HashTable.prototype.remove = function(k){
     var tuple = bucket[j];
     if(tuple[0] === k) {
       bucket.splice(j, 1);
+      this._count--;
+      if (this._count < this._limit * 0.25) {
+        this._resize(this._limit / 2);
+      }
       return tuple[1];
     }
   }
   return null;
 };
+
+HashTable.prototype._resize = function(newLimit) {
+  var oldStorage = this._storage;
+  this._storage = LimitedArray(newLimit);
+  this._limit = newLimit;
+  this._count = 0;
+
+  oldStorage.each(function(bucket) {
+    if (!bucket) {
+      return;
+    }
+
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      this.insert(tuple[0], tuple[1]);
+    }
+  }.bind(this));
+};
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
