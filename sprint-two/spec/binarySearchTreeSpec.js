0e239b932149a3b243df06729c59313ceb3823b4
Cfeusier <cfeusier@gmail.com>
2014-12-13 14:11:45 -0800

    Finish basic requirements with hashtable edits

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -34,7 +34,6 @@ describe('binarySearchTree', function() {
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
     binarySearchTree.depthFirstLog(func);
-    console.log(array);
     expect(array).to.eql([5,2,3]);
   });
 });
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,22 +5,44 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var obj = {};
-  obj[k] = v;
-  if(!this._storage.get(i)) {
-    var firstBucket = [obj];
-    this._storage.set(i, firstBucket);
-  } else {
-    this._storage.get(i).push(obj);
+  var bucket = this._storage.get(i);
+
+  if(!bucket) {
+    bucket = [];
+    this._storage.set(i, bucket);
   }
+
+  var found = false;
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+
+    // found matching key
+    if (tuple[0] === k) {
+      // update key with new value
+      tuple[1] = v;
+      found = true;
+      break;
+    }
+  }
+
+  // no matching key found
+  if (!found) {
+    bucket.push([k,v]);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
+  if(!bucket) {
+    return null;
+  }
   for (var j = 0; j < bucket.length; j++) {
-    if(bucket[j][k]) {
-      return bucket[j][k];
+    var tuple = bucket[j];
+    if(tuple[0] === k) {
+      return tuple[1];
     }
   }
   return null;
@@ -29,11 +51,19 @@ HashTable.prototype.retrieve = function(k){
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
+
+  if (!bucket) {
+    return null;
+  }
+
   for (var j = 0; j < bucket.length; j++) {
-    if(bucket[j][k]) {
+    var tuple = bucket[j];
+    if(tuple[0] === k) {
       bucket.splice(j, 1);
+      return tuple[1];
     }
   }
+  return null;
 };
 
 /*
